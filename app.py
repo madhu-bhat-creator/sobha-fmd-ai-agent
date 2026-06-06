47e404cdb69b834fa5382fa18f4f548519cfb8e9
madhu-bhat-creator <madhu@madhubhat.com>
2026-06-06 23:59:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
 
 st.title("Sobha Windsor FMD Agent")
 
-st.write("AI Powered FMD Operations Assistant")
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=scope
+)
+
+client = gspread.authorize(creds)
+
+st.success("Google Authentication Successful")
